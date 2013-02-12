51393aa08baad9d492fb9e5c4a33c70266ce3b1e
Silvan Spross <silvan.spross@gmail.com>
2013-02-12 16:08:51 +0100

    renamed files

--- __init__.py
+++ __init__.py
@@ -3,16 +3,16 @@ import unittest
 
 class ProblemTests(unittest.TestCase):
     def test_problem_1(self):
-        from problem_1 import func
+        from p001 import func
         self.assertEqual(func(10), 23)
         self.assertEqual(func(1000), 233168)
 
     def test_problem_2(self):
-        from problem_2 import func
+        from p002 import func
         self.assertEqual(func(4000000), 4613732)
 
     def test_problem_3(self):
-        from problem_3 import func
+        from p003 import func
         self.assertEqual(func(13195), 29)
         self.assertEqual(func(600851475143), 6857)
 
