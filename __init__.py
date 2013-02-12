fc16b292b8e7a2fae8e2e8c23daf186e81947105
Silvan Spross <silvan.spross@gmail.com>
2013-02-12 10:52:49 +0100

    added solutions for problem 2

--- __init__.py
+++ __init__.py
@@ -7,6 +7,10 @@ class ProblemTests(unittest.TestCase):
         self.assertEqual(func(10), 23)
         self.assertEqual(func(1000), 233168)
 
+    def test_problem_2(self):
+        from problem_2 import func
+        self.assertEqual(func(4000000), 4613732)
+
 
 if __name__ == '__main__':
     unittest.main()
---
+++ problem_2.py
@@ -0,0 +1,8 @@
+def fibs(maxval):
+    curr, next = 0, 1
+    while curr < maxval:
+        yield curr
+        curr, next = next, curr + next
+
+def func(maxval):
+    return sum([v for v in fibs(maxval) if v % 2 == 0])
