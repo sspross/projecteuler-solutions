3b54e5b338e89993635077bf9a2ad03c8d49807b
Silvan Spross <silvan.spross@gmail.com>
2013-02-14 13:37:30 +0100

    improved solution 2

--- p002.py
+++ p002.py
@@ -1,9 +1,10 @@
-def fibs(maxval):
+def even_fibs(below):
     curr, next = 0, 1
-    while curr < maxval:
-        yield curr
+    while curr < below:
+        if curr % 2 == 0:
+            yield curr
         curr, next = next, curr + next
 
 
-def func(maxval):
-    return sum([v for v in fibs(maxval) if v % 2 == 0])
+def func(below):
+    return sum(even_fibs(below))
