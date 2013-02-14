c5b2c43957dc1c700c3bfee04fe68195195b90d8
Silvan Spross <silvan.spross@gmail.com>
2013-02-14 08:24:24 +0100

    improved solution for problem 1

--- p001.py
+++ p001.py
@@ -1,2 +1,13 @@
+def nums(below):
+    a, b = 3, 5
+    while a < below or b < below:
+        if a < b:
+            yield a
+            a += 3
+        else:
+            yield b
+            b += 5
+
+
 def func(below):
-    return sum([v for v in xrange(below) if v % 3 == 0 or v % 5 == 0])
+    return sum(set([num for num in nums(below)]))
